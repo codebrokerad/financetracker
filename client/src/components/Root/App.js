d2f88ac633b92c3cc53839a5a8bf45c47c61c303
codebroker <utuman@windowslive.com>
2020-12-06 15:16:00 +0100

    frontend code added

---
+++ client/src/components/Root/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+
+import Signup from "../Auth/Signup";
+import Login from "../Auth/Login";
+import ProtectedRoute from "../Auth/ProtectedRoute";
+
+import Navbar from "../Navbar/Navbar";
+import AuthService from "../../services/auth-service";
+
+function App() {
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  const service = new AuthService();
+
+  // Function to help fetch a logged in user
+  const fetchUser = () => {
+    if (loggedInUser === null) {
+      service
+        .isAuthenticated()
+        .then((response) => {
+          setLoggedInUser(response);
+        })
+        .catch((err) => {
+          setLoggedInUser(false);
+        });
+    }
+  };
+
+  // Function to help get the loggedIn user
+  const getLoggedInUser = (userObject) => {
+    setLoggedInUser(userObject);
+  };
+
+  // Run to check if user is authenticated
+  fetchUser();
+
+  return loggedInUser ? (
+    <section className="App">
+      <Navbar userInSession={loggedInUser} getUser={getLoggedInUser} />
+      <Switch>
+        <ProtectedRoute
+          user={loggedInUser}
+          path="/stocks"
+          component=""
+        />
+      </Switch>
+    </section>
+  ) : (
+    <section className="App">
+      <Navbar userInSession={loggedInUser} getUser={getLoggedInUser} />
+
+      <Switch>
+        <Route
+          exact
+          path="/signup"
+          render={() => <Signup getUser={getLoggedInUser} />}
+        />
+        <Route
+          exact
+          path="/"
+          render={() => <Login getUser={getLoggedInUser} />}
+        />
+        <ProtectedRoute
+          user={loggedInUser}
+          path="/projects"
+          component=""
+        />
+      </Switch>
+    </section>
+  );
+}
+
+export default App;
---
+++ client/src/services/auth-service.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+// Contains all the methods and also we need to use the create method of axios to build a new instance
+class AuthService {
+  constructor() {
+    this.service = axios.create({
+      baseURL: process.env.REACT_APP_BASE_URL,
+      withCredentials: true, // indicates whether or not cross-site Access-Control requests should be made using credentials
+    });
+  }
+
+  // Method to use in our SignUp component
+  signup = (username, password) => {
+    return this.service
+      .post("http://localhost:5001/api/signup", { username, password })
+      .then((response) => response.data);
+  };
+
+  // Method to use in our Login component
+  login = (username, password) => {
+    return this.service
+      .post("http://localhost:5001/api/login", { username, password })
+      .then((response) => response.data);
+  };
+
+  // Method to use to see if user is authenticated
+  isAuthenticated = () => {
+    return this.service
+      .get("http://localhost:5001/api/loggedin")
+      .then((response) => response.data);
+  };
+
+  // Method to use for logging our user out
+  logout = () => {
+    return this.service
+      .post("http://localhost:5001/api/logout", {})
+      .then((response) => response.data);
+  };
+}
+
+export default AuthService;
